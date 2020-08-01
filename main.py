69a10538b4e44f1afa96fe110b9d7a896c7d9a9f
NikitaKolotushkin <nikitaplay1000@gmail.com>
2020-08-01 20:28:58 +0400

    Add a functional

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello!")
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+def RuntimeMeter(function):
+    import time
+
+    def wrapper():
+        start = time.time()
+        function()
+        end = time.time()
+        print '[*] Execution time: {0}'.format(start - end)
+
+    return wrapper
